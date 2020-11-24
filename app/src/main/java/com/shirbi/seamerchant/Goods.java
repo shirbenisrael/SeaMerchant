b45c67b5ef29aca974c0facf7c056ea18dd6a2bf
Shir Ben-Israel <shirbenisrael@gmail.com>
2020-11-24 13:39:55 +0200

    [New Feature] Add price table generator.

--- app/src/main/java/com/shirbi/seamerchant/Goods.java
+++ app/src/main/java/com/shirbi/seamerchant/Goods.java
@@ -1,11 +1,18 @@
 package com.shirbi.seamerchant;
 
+import java.util.Random;
+
 public enum Goods {
     WHEAT(0),
     OLIVES(1),
     COPPER(2);
 
     public static final int NUM_GOODS_TYPES = 3;
+    private Random rand = new Random();
+
+    static private int[] multiplier = {5,50,100};
+    static private int[] minimum = {7, 7, 25};
+    static private int[] numValues = {8, 7, 11};
 
     private final int value;
     Goods(int value){
@@ -14,5 +21,9 @@ public enum Goods {
 
     public int getValue() {
         return value;
+    }
+
+    public int generateRandom() {
+        return (rand.nextInt(numValues[value]) + minimum[value]) * multiplier[value];
     }
 }
--- app/src/main/java/com/shirbi/seamerchant/PriceTable.java
+++ app/src/main/java/com/shirbi/seamerchant/PriceTable.java
@@ -10,4 +10,16 @@ public class PriceTable {
             }
         }
     }
+
+    public void generateRandomPrices() {
+        for (int[] state_prices : prices) {
+            for (Goods goods: Goods.values()) {
+                state_prices[goods.getValue()] = goods.generateRandom();
+            }
+        }
+    }
+
+    public int getPrice(State state, Goods goods) {
+        return prices[state.getValue()][goods.getValue()];
+    }
 }
