a3a4a6300cc52b34942d1b0c79680a5f52ceb140
Shir Ben-Israel <shirbenisrael@gmail.com>
2020-11-27 01:16:47 +0200

    [New Feature] sail window: animate sail route.
    Currently in straight line.

--- app/src/main/java/com/shirbi/seamerchant/FrontEndSail.java
+++ app/src/main/java/com/shirbi/seamerchant/FrontEndSail.java
@@ -3,11 +3,20 @@ package com.shirbi.seamerchant;
 import android.graphics.Point;
 import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.IdRes;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FrontEndSail extends FrontEndGeneric {
+    private Timer mTimer;
+    private TimerTask mTimerTask;
+    private Point mMapSize;
+    private float mProgress;
+
     public FrontEndSail(MainActivity activity) {
         super(activity);
     }
@@ -19,8 +28,8 @@ public class FrontEndSail extends FrontEndGeneric {
         return size;
     }
 
-    private Point getMapSize() {
-        Point mapSize = getWindowSize(); // need to adjust y
+    private void calculateMapSize() {
+        mMapSize = getWindowSize(); // need to adjust y
 
         RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.map_container);
         float weight = ((LinearLayout.LayoutParams)relativeLayout.getLayoutParams()).weight;
@@ -32,29 +41,66 @@ public class FrontEndSail extends FrontEndGeneric {
             totalWeights += ((LinearLayout.LayoutParams)view.getLayoutParams()).weight;
         }
 
-        mapSize.y *= weight / totalWeights;
-
-        return mapSize;
+        mMapSize.y *= weight / totalWeights;
     }
 
-    public void initSailRoute() {
+    void putObjectOnMap(@IdRes int id, float x, float y, float width, float height) {
         RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.map_container);
-        View destinationAnchor = findViewById(R.id.destination);
+        View object = findViewById(id);
 
         RelativeLayout.LayoutParams params =
-                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                new RelativeLayout.LayoutParams((int)(width * mMapSize.x), (int)(height * mMapSize.x));
 
-        Point mapSize = getMapSize();
-        int windowWidth = mapSize.x;
-        int windowHeight = mapSize.y;
-
-        params.leftMargin = (int)(mLogic.mSail.mDestination.toLocationX() * windowWidth);
-        params.topMargin = (int)(mLogic.mSail.mDestination.toLocationY() * windowHeight);
+        params.leftMargin = (int)(x * mMapSize.x);
+        params.topMargin = (int)(y * mMapSize.y);
         params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
         params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
 
-        relativeLayout.removeView(destinationAnchor);
-        relativeLayout.addView(destinationAnchor, params);
+        relativeLayout.removeView(object);
+        relativeLayout.addView(object, params);
     }
+
+    public void initSailRoute() {
+        calculateMapSize();
+        putObjectOnMap(R.id.destination,
+                mLogic.mSail.mDestination.toLocationX(),
+                mLogic.mSail.mDestination.toLocationY(),
+                0.05f, 0.10f);
+
+        mProgress = 0.0f;
+
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+        mTimer = new Timer();
+        mTimerTask = (new TimerTask() {
+            @Override
+            public void run() {
+                mActivity.runOnUiThread(mTimerTick);
+            }
+        });
+
+        mTimer.schedule(mTimerTask,0, 100);
+    }
+
+    Runnable mTimerTick = new Runnable() {
+        public void run() {
+            float locationX = mProgress * mLogic.mSail.mDestination.toLocationX() +
+                    (1.0f- mProgress) * mLogic.mSail.mSource.toLocationX();
+
+            float locationY = mProgress * mLogic.mSail.mDestination.toLocationY() +
+                    (1.0f- mProgress) * mLogic.mSail.mSource.toLocationY();
+
+            putObjectOnMap(R.id.circle_on_map,
+                    locationX,
+                    locationY,
+                    0.05f, 0.05f);
+
+            mProgress += 0.02f;
+
+            if (mProgress > 1.0f) {
+                mProgress = 0.0f;
+            }
+        }
+    };
 }
